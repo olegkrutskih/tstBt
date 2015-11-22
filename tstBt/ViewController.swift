434aae26c6ac7aa2526691c4b2f75b0406967041
Олег Круцких <oleg.krutskih@icloud.com>
2015-11-22 17:15:44 +0300

    Initial Commit

---
+++ tstBt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tstBt
+//
+//  Created by Круцких Олег on 22.11.15.
+//  Copyright © 2015 Круцких Олег. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
